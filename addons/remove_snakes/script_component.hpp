7c5952221efa68a1e9211fcbcfac4b5d8bc7c7c7
Timi007 <timi007gp@gmail.com>
2019-09-14 16:33:11 +0200

    add remove snakes component

---
+++ addons/remove_snakes/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT remove_snakes
+#define COMPONENT_BEAUTIFIED Remove_Snakes
+#include "\z\mts_enhanced\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define CBA_DEBUG_SYNCHRONOUS
+
+#ifdef DEBUG_ENABLED_REMOVE_SNAKES
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_REMOVE_SNAKES
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_REMOVE_SNAKES
+#endif
+
+#include "\z\mts_enhanced\addons\main\script_macros.hpp"
