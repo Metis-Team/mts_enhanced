55f9d37443929ab2dc460a6c221e17d0e040d643
Timi007 <timi007gp@gmail.com>
2019-09-16 17:05:56 +0200

    improve natures config inheritance (#15)
    * remove obsolete rocks
    
    * improve config inheritance & provide BWC for rocks

--- addons/nature/CfgEditorCategories.hpp
+++ addons/nature/CfgEditorCategories.hpp
@@ -1,9 +1,3 @@
-class CfgEditorCategories {
-    class GVAR(faction) {
-        displayName = CSTRING(Faction);
-    };
-};
-
 class CfgEditorSubcategories {
     class GVAR(bush_faction) {
         displayName = CSTRING(Bush_Faction);
@@ -13,8 +7,5 @@ class CfgEditorSubcategories {
     };
     class GVAR(tree_faction) {
         displayName = CSTRING(Tree_Faction);
-    };
-    class GVAR(rock_faction) {
-        displayName = CSTRING(Rock_Faction);
     };
 };
