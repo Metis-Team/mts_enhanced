e32e581c314eb47a1720711b72f764b02733a3bd
Timi007 <timi007gp@gmail.com>
2020-03-19 19:55:29 +0100

    untabify

--- addons/remove_snakes/CfgWorlds.hpp
+++ addons/remove_snakes/CfgWorlds.hpp
@@ -1,39 +1,39 @@
 class CfgWorlds {
-	class CAWorld;
+    class CAWorld;
 
-	class Altis: CAWorld {
-		class AmbientA3 {
-			class Radius40_60 {
-				class Species {
-					class Snake_random_F {
-						maxWorldCount = 0;
-					};
-				};
-			};
-		};
-	};
+    class Altis: CAWorld {
+        class AmbientA3 {
+            class Radius40_60 {
+                class Species {
+                    class Snake_random_F {
+                        maxWorldCount = 0;
+                    };
+                };
+            };
+        };
+    };
 
-	class Stratis: CAWorld {
-		class AmbientA3 {
-			class Radius40_60 {
-				class Species {
-					class Snake_random_F {
-						maxWorldCount = 0;
-					};
-				};
-			};
-		};
-	};
+    class Stratis: CAWorld {
+        class AmbientA3 {
+            class Radius40_60 {
+                class Species {
+                    class Snake_random_F {
+                        maxWorldCount = 0;
+                    };
+                };
+            };
+        };
+    };
 
     class Malden: CAWorld {
-		class AmbientA3 {
-			class Radius40_60 {
-				class Species {
-					class Snake_random_F {
-						maxWorldCount = 0;
-					};
-				};
-			};
-		};
-	};
+        class AmbientA3 {
+            class Radius40_60 {
+                class Species {
+                    class Snake_random_F {
+                        maxWorldCount = 0;
+                    };
+                };
+            };
+        };
+    };
 };
