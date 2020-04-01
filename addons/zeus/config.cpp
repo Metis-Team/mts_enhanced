58629c764f4976892beeb7eea6d77868012deaf6
Phil <erodond@gmail.com>
2020-04-02 01:01:40 +0200

    add common and dialog dependecy

--- addons/zeus/config.cpp
+++ addons/zeus/config.cpp
@@ -6,7 +6,7 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"mts_main", "zen_custom_modules", "ace_medical", "ace_zeus"};
+        requiredAddons[] = {"mts_main", "zen_custom_modules", "zen_dialog", "zen_common", "ace_medical", "ace_zeus"};
         author = "";
         authors[] = {"PhILoX", "Timi007"};
         VERSION_CONFIG;
