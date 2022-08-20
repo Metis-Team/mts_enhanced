8a462347d049d1a6036b78220271bc8c7f415f54
Timi007 <timi007gp@gmail.com>
2022-08-20 19:33:00 +0200

    Add bush cutter

--- addons/common/initSettings.hpp
+++ addons/common/initSettings.hpp
@@ -3,6 +3,7 @@
 #define TRAILING_DECIMALS 0
 
 #define DEFAULT_GRASSCUTTER_DURATION 5
+#define DEFAULT_BUSHCUTTER_DURATION 10
 
 // Grasscutter: On/off
 [
@@ -35,5 +36,26 @@
     [LLSTRING(grasscutter_duration), LLSTRING(grasscutter_duration_tooltip)],
     [LELSTRING(main,category), LLSTRING(grasscutter_subCategory)],
     [MIN_DURATION, MAX_DURATION, DEFAULT_GRASSCUTTER_DURATION, TRAILING_DECIMALS],
+    1 // Server setting
+] call CBA_fnc_addSetting;
+
+
+// Bushcutter: On/off
+[
+    QGVAR(bushcutter_enabled),
+    "CHECKBOX",
+    [LLSTRING(bushcutter), LLSTRING(bushcutter_tooltip)],
+    [LELSTRING(main,category), LLSTRING(bushcutter_subCategory)],
+    true,
+    0
+] call CBA_fnc_addSetting;
+
+// Bushcutter: Time it takes to cut down a bush
+[
+    QGVAR(bushcutter_duration),
+    "SLIDER",
+    [LLSTRING(bushcutter_duration), LLSTRING(bushcutter_duration_tooltip)],
+    [LELSTRING(main,category), LLSTRING(bushcutter_subCategory)],
+    [MIN_DURATION, MAX_DURATION, DEFAULT_BUSHCUTTER_DURATION, TRAILING_DECIMALS],
     1 // Server setting
 ] call CBA_fnc_addSetting;
