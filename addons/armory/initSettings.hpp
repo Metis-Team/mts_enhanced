8dcf19927f8a44ef9bbfff0c46c6a2d61a6603bb
Phil <erodond@gmail.com>
2023-09-21 16:25:56 +0200

    use already existing disaplayName and remove subCategory

--- addons/armory/initSettings.hpp
+++ addons/armory/initSettings.hpp
@@ -2,7 +2,7 @@
     QGVAR(cba_settings_actions),
     "LIST",
     [LLSTRING(cba_settings_actions), LLSTRING(cba_settings_actions_tooltip)],
-    [LELSTRING(main,category), LLSTRING(subCategory)],
+    [LELSTRING(main,category), LLSTRING(displayName)],
     [["ace_interaction","scroll_menu"],[LLSTRING(cba_settings_actions_ace_interaction), LLSTRING(cba_settings_actions_scroll_menu)],0],
     1,
     {}
@@ -12,7 +12,7 @@
     QGVAR(cba_settings_playerDBConnection),
     "CHECKBOX",
     [LLSTRING(cba_settings_playerDBConnection), LLSTRING(cba_settings_playerDBConnection_tooltip)],
-    [LELSTRING(main,category), LLSTRING(subCategory)],
+    [LELSTRING(main,category), LLSTRING(displayName)],
     false,
     0,
     {},
