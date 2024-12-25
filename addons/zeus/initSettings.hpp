15aaa8851b9e0c86b2d6ee6b0b0e51d57110a39e
Timi007 <timi007gp@gmail.com>
2024-12-25 17:27:10 +0100

    Zeus - Add setting to customize color of 3DEN comments & change font (#121)
    * Change color and font of comments
    
    * Optimize 3DEN comments and make color customizable
    
    * Fix build error
    
    * Optimize

--- addons/zeus/initSettings.hpp
+++ addons/zeus/initSettings.hpp
@@ -22,6 +22,14 @@
     [LLSTRING(3DENComments), LLSTRING(3DENCommentsTooltip)],
     [LELSTRING(main,category), LLSTRING(subCategory)],
     true,
-    0,
-    {}
+    0
+] call CBA_fnc_addSetting;
+
+[
+    QGVAR(3DENCommentsColor),
+    "COLOR",
+    [LLSTRING(3DENCommentsColor), LLSTRING(3DENCommentsColorTooltip)],
+    [LELSTRING(main,category), LLSTRING(subCategory)],
+    [0, 1, 0.75, 1],
+    0
 ] call CBA_fnc_addSetting;
