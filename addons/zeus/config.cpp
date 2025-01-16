34d0158a8732891e4e371dce6c2398779f8ae0ab
Timi007 <timi007gp@gmail.com>
2025-01-16 22:11:15 +0100

    Remove removing watermark (#125)

--- addons/zeus/config.cpp
+++ addons/zeus/config.cpp
@@ -26,15 +26,3 @@ class CfgPatches {
 #include "CfgEventHandlers.hpp"
 #include "CfgAmmo.hpp"
 #include "Cfg3DEN.hpp"
-
-class RscPicture;
-class RscDisplayCurator {
-    class Controls {
-        class Watermark: RscPicture {
-            text = "";
-            colorText[] = {1,1,1,0};
-            w = 0;
-            h = 0;
-        };
-    };
-};
