6e52fed40c5143d809c512804bab8b9250801026
Timi007 <timi007gp@gmail.com>
2025-07-26 14:36:45 +0200

    Add enhanced zeus comments

---
+++ addons/zeus_comments/config.cpp
@@ -0,0 +1,20 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"mts_main", "zen_dialog"};
+        author = ECSTRING(main,Author);
+        authors[] = {"Timi007"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgEventHandlers.hpp"
+#include "Cfg3DEN.hpp"
+#include "CfgContext.hpp"
+#include "gui.hpp"
